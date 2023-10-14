380af021ea8afbd90d61d88abbcdd494a931b532
RAFAELLAMATOS <rafaella.matos@gmail.com>
2023-10-14 11:16:34 -0300

    responsive navbar

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".hamburguer-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
+
 function getCurrentYear() {
     return new Date().getFullYear();
 }
