389c8334fe60aa25d48d1453d399b308eeb8ad77
RAFAELLAMATOS <rafaella.matos@gmail.com>
2023-10-14 12:00:22 -0300

    Profile section

--- script.js
+++ script.js
@@ -9,5 +9,25 @@ function getCurrentYear() {
     return new Date().getFullYear();
 }
 
+function openResume(){
+    window.open('./assets/file/Rafaella Matos - Resume.pdf')
+}
+
+function goContact(){
+    location.href='./#contact'
+}
+
+function goLinkedin(){
+    location.href = "https://www.linkedin.com/in/rafaella-matos/"
+}
+
+function goGithub(){
+    location.href = "https://www.github.com/RAFAELLAMATOS"
+}
+
+function goInstagram(){
+    location.href = "https://www.instagram.com/techparamulheres"
+}
+
 // Update the footer year
 document.getElementById('footer-year').textContent = `© ${getCurrentYear()} Rafaella Matos. All rights reserved.`;
